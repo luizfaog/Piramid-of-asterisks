dd219b5003b1282eed7a270c494c5a539bd3822f
luizfaog <luiz.felipe008@hotmail.com>
2022-02-25 21:26:38 -0300

    Piramide-de-asteriscos

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace piramideAsterisco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linhas;
+
+            Console.WriteLine("Informe a qntd de linhas presentes na sua piramide: ");
+            linhas = int.Parse(Console.ReadLine());
+            for (int linhaAtual = 1; linhaAtual <= linhas; linhaAtual++)
+            {
+                for (int j = 0; j < linhas - linhaAtual; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < (linhaAtual * 2) - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
